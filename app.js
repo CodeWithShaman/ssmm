283daced870e16c826f52b2c0739277b8c9e77ae
CodeWithShaman <shamanaliteevino@gmail.com>
2024-09-05 03:36:15 +0500

    Talha is herr.......

---
+++ app.js
@@ -0,0 +1,93 @@
+// alert("Hello World")
+// console.log("My Name Is Shaman");
+// document.write("Shaman Ali");
+// window.alert("Shaman");
+// var name = "shaman"; this is the string
+// var age = 20;
+// alert(age);
+
+// if 20 is greater then 40 that output is else mean false  
+
+// var x = 20;
+
+// if (x > 40){
+//     document.write("x is greater");
+// }else{
+//     document.write("x is smaller");
+// }
+
+// if 100 is == equal 100 that output is ture
+// var y = 100;
+
+// if(y ==  100){
+//     document.write("y is same");
+// }else{
+//     document.write("y is not same")
+// }
+
+// if '100' in string  ===  100 that output is false
+// var y = '100';
+
+// if(y ===  100){
+//     document.write("y is same");
+// }else{
+//     document.write("y is not same")
+// }
+
+// var myName = 'Shaman';
+// var gender = "Male";
+
+// if(gender == "Male"){
+//     document.write("Hello Mr." + myName);
+// }else{
+//     document.write("Hello Miss." + myName);
+// }
+
+// var myName = 'Shaman';
+// var gender = "Female";
+
+// if(gender == "Male"){
+//     document.write("Hello Mr." + myName);
+// }else{
+//     document.write("Hello Miss." + myName);
+// }
+
+// var x = 15;
+
+// if(x > 30){
+//     document.write("x is greater");
+// }else{
+//     document.write("x is smaller");
+// }
+
+// var x = 100;
+
+// if(x == 100){
+//     document.write("x is same");
+// }else{
+//     document.write("x is not same");
+// }
+
+// var x = '100';
+
+// if(x === 100){
+//     document.write("x is same");
+// }else{
+//     document.write("x is not same");
+// }
+
+var per = 1;
+
+if(per >= 80 && per <= 100){
+    document.write("You Are in A1");
+}else if(per >= 60 && per < 80){
+    document.write("You Are in A");
+}else if(per >= 45 && per < 60){
+    document.write("You Are in B");
+}else if(per >= 33 && per < 45){
+    document.write("You Are in C");
+}else if(per > 33){
+    document.write("You Are Fail");
+}else{
+    document.write("Please Inter Valid Percentage.");
+}
